3067b6d4c3a736cdb63f9999d4d9068ab3716f25
izabella-m <izabella.pessoa03@outlook.com>
2025-05-10 23:53:48 -0300

    chore: adiciona dependências e instâncias do v-mask

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import '@mdi/font/css/materialdesignicons.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import '@mdi/font/css/materialdesignicons.css';
+import App from './App.vue';
+import router from './router';
+import { VueMaskDirective } from 'v-mask';
 
-createApp(App).mount('#app')
+const vMaskV2 = VueMaskDirective;
+const vMaskV3 = {
+  beforeMount: vMaskV2.bind,
+  updated: vMaskV2.componentUpdated,
+  unmounted: vMaskV2.unbind,
+};
+
+const app = createApp(App);
+app.directive('mask', vMaskV3);
+app.use(router);
+app.mount('#app');
