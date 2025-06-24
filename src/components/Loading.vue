0cbcd6df288f49a3ab4c5201d1dae47831f587ea
izabella-m <izabella.pessoa03@outlook.com>
2025-06-23 21:06:22 -0300

    feat: adiciona animação de carregamento

---
+++ src/components/Loading.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="containerCarregamento">
+    <div class="animacaoCarregamento"></div>
+    <p>Carregando...</p>
+  </div>
+</template>
+
+<style scoped>
+.containerCarregamento {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.animacaoCarregamento {
+  width: 40px;
+  height: 40px;
+  border: 4px solid #ccc;
+  border-top-color: #f8a953;
+  border-radius: 50%;
+  animation: spin 0.8s linear infinite;
+}
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
